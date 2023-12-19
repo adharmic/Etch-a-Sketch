ebc92c23b655bed9209e940207392c876218523b
Adithya Ajith <adithyajith925@gmail.com>
2023-12-19 11:06:23 -0600

    Implement grid resizability and clear button

--- sketch.js
+++ sketch.js
@@ -1,17 +1,40 @@
 const SKETCH_CONTAINER = document.querySelector('#sketch-container');
 SKETCH_CONTAINER.style.backgroundColor = `gray`;
+const SIZE_TEXT = document.querySelector('#size-text');
 
-console.log(SKETCH_CONTAINER.clientHeight);
+const CLEAR_BUTTON = document.querySelector('#clear-grid');
+CLEAR_BUTTON.addEventListener("click", function() {
+    renderGrid();
+});
 
-let side_length = 16;
+const SIZE_BUTTON = document.querySelector('#submit-size');
+SIZE_BUTTON.addEventListener("click", function() {
+    side_length = Number(SIZE_TEXT.value);
+    renderGrid();
+});
 
-for (let index = 0; index < side_length ** 2; index++) {
+const DEFAULT_LENGTH = 16;
+let side_length = DEFAULT_LENGTH;
 
-    let GRID_ELEMENT = document.createElement("div");
-    GRID_ELEMENT.addEventListener("mouseover", function(e) {
-        this.style.backgroundColor = "black";
-    });
-    GRID_ELEMENT.style.width = `${Math.floor(SKETCH_CONTAINER.clientWidth / side_length)}px`;
-    GRID_ELEMENT.style.height = `${Math.floor(SKETCH_CONTAINER.clientHeight / side_length)}px`;
-    SKETCH_CONTAINER.appendChild(GRID_ELEMENT);
+function clearGrid() {
+    while (SKETCH_CONTAINER.firstChild) {
+        SKETCH_CONTAINER.removeChild(SKETCH_CONTAINER.firstChild);
+    }
 }
+
+function renderGrid(num_sides = side_length) {
+    clearGrid();
+
+    for (let index = 0; index < num_sides ** 2; index++) {
+
+        let GRID_ELEMENT = document.createElement("div");
+        GRID_ELEMENT.addEventListener("mouseover", function(e) {
+            this.style.backgroundColor = "black";
+        });
+        GRID_ELEMENT.style.width = `${Math.floor(SKETCH_CONTAINER.clientWidth / num_sides)}px`;
+        GRID_ELEMENT.style.height = `${Math.floor(SKETCH_CONTAINER.clientHeight / num_sides)}px`;
+        SKETCH_CONTAINER.appendChild(GRID_ELEMENT);
+    }
+}
+
+renderGrid();
