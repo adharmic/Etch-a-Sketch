989e22a5a091a053e2fd58f2197c8e896b94cd2e
Adithya Ajith <adithyajith925@gmail.com>
2023-12-19 13:34:17 -0600

    Add drawing options and reset grid on window resize

--- sketch.js
+++ sketch.js
@@ -1,6 +1,26 @@
 const DEFAULT_LENGTH = 16;
 let side_length = DEFAULT_LENGTH;
 
+const MODE_INFO = document.querySelector('#mode-info');
+
+const NORMAL = 0;
+const RAINBOW = 1;
+const OPACITY = 2;
+
+const NORMAL_BUTTON = document.querySelector('#normal-button');
+NORMAL_BUTTON.addEventListener("click", () => {
+    MODE_INFO.textContent = "Current Mode: Normal";
+    renderGrid(NORMAL);
+});
+
+const RAINBOW_CHECKBOX = document.querySelector('#rainbow-toggle');
+
+const OPACITY_BUTTON = document.querySelector('#opacity-button');
+OPACITY_BUTTON.addEventListener("click", () => {
+    MODE_INFO.textContent = "Current Mode: Opacity";
+    renderGrid(OPACITY);
+});
+
 const SKETCH_CONTAINER = document.querySelector('#sketch-container');
 SKETCH_CONTAINER.style.backgroundColor = `gray`;
 const SIZE_TEXT = document.querySelector('#size-text');
@@ -39,22 +59,41 @@ function clearGrid() {
     }
 }
 
-function renderGrid(num_sides = side_length) {
+function renderGrid(mode = NORMAL) {
     clearGrid();
 
-    for (let index = 0; index < num_sides ** 2; index++) {
+    for (let index = 0; index < side_length ** 2; index++) {
 
         let GRID_ELEMENT = document.createElement("div");
+        if (mode === OPACITY) {
+            GRID_ELEMENT.style.opacity = 0.0;
+        }
         GRID_ELEMENT.addEventListener("mouseover", function(e) {
-            this.style.backgroundColor = "black";
+            if (RAINBOW_CHECKBOX.checked) {
+                this.style.backgroundColor = "#" + Math.floor(Math.random()*16777215).toString(16);
+            }
+            else {
+                this.style.backgroundColor = "black";
+            }
+            switch(mode) {
+                case OPACITY:
+                    let currentOpac = this.style.opacity;
+                    let newOpac = Number(currentOpac) + .1;
+                    this.style.opacity = newOpac;
+                    break;
+                default:
+                    break;
+            }
         });
-        GRID_ELEMENT.style.width = `${Math.floor(SKETCH_CONTAINER.clientWidth / num_sides)}px`;
-        GRID_ELEMENT.style.height = `${Math.floor(SKETCH_CONTAINER.clientHeight / num_sides)}px`;
+        GRID_ELEMENT.style.width = `${Math.floor(SKETCH_CONTAINER.clientWidth / side_length)}px`;
+        GRID_ELEMENT.style.height = `${Math.floor(SKETCH_CONTAINER.clientHeight / side_length)}px`;
         SKETCH_CONTAINER.appendChild(GRID_ELEMENT);
     }
     GRID_INFO.style.color = "black";
     GRID_INFO.textContent = `Current Grid Size: ${side_length}x${side_length}`;
     SIZE_TEXT.value = ``;
 }
+
+window.addEventListener('resize', renderGrid);
 
 renderGrid();
